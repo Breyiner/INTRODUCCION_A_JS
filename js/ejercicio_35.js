55c3fa6ce413a383127ecae9a5a48ea284291090
Breyiner <breyneracosta7@gmail.com>
2024-12-09 10:07:48 -0500

    ejercicio 35

---
+++ js/ejercicio_35.js
@@ -0,0 +1,7 @@
+let llaves = 11500;
+let bomba = 1168000;
+let pernos = 87000;
+let sobrante =  91000;
+
+let calculo = (5 * llaves) + bomba + (3 * pernos) + sobrante;
+console.log(`El jefe de obra tenía $${calculo}`);
