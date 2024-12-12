b6fe25f34809b8bfceeff103d1ea2d8a081e291e
Breyiner <breyneracosta7@gmail.com>
2024-12-12 14:46:28 -0500

    ejercicio 49 arreglo

--- js/ejercicio_49.js
+++ js/ejercicio_49.js
@@ -1,17 +1,25 @@
 const solicitar = (medida) => {
     while (true) {
         try {
-            let numero = parseFloat(prompt(`Ingrese la ${medida}:`));
-            if (Number.isNaN(numero)) throw new Error("Ingrese un dato válido");
+            let numero = parseFloat(prompt(`Ingrese el ${medida} del rectángulo:`));
+            if (numero == 0 || Number.isNaN(numero)) throw new Error("Ingrese un dato válido");
             else return numero;
         } catch (error) {
             console.log(error.message);
         }
     }
 }
+const areaRectangulo = (ancho, largo) => {
+    try {
+        if (ancho < 0 || largo < 0) throw new Error("Los datos deben ser positivos");
+        let area = ancho * altura;
+        alert(`El área del rectangulo de ancho ${ancho} y largo ${largo} es ${area}`);
+    } catch (error) {
+        alert(error.message);
+    }
+}
 
-let presion = solicitar("presión");
-let temperatura = solicitar("temperatura");
+let ancho = solicitar("ancho");
+let largo = solicitar("largo");
 
-if (presion > 200 || temperatura > 100) alert("Alarma");
-else alert("Normal");
+areaRectangulo(ancho, largo)
