66d7e9c1d01667b29cb11b0c02466fd6802c59f5
Breyiner <breyneracosta7@gmail.com>
2024-12-12 18:59:53 -0500

    ejercicio 64 arreglo

--- js/ejercicio_64.js
+++ js/ejercicio_64.js
@@ -12,6 +12,11 @@ const solicitar = () => {
 
 let numero = solicitar();
 
+let impares = "";
 for (let i = 1; i <= numero; i++) {
-    if (i % 2 !== 0) console.log(i);
+    if (i % 2 !== 0) {
+        impares += i;
+        if (i + 2 <= numero) impares += ",";
+    }
 }
+console.log(impares);
